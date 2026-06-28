4a5f3e8cdc44ca4763bbbf952d73c4bec1c7f3e7
AadityaAcharya156 <aadityaacharya156@gmail.com>
2026-06-28 21:29:39 +0545

    Update server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log('Hello world')
+// console.log(global)
+console.log('Hello world2')
+
+const os = require('os')
+console.log(os.type())
+console.log(os.version())
+console.log(os.homedir())
+
+console.log(__dirname)
+console.log(__filename)
