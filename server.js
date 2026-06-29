ea37c4476580588c6ddb85affe12469af0d3592e
AadityaAcharya156 <aadityaacharya156@gmail.com>
2026-06-29 12:42:59 +0545

    ..

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ console.log('Hello world')
 // console.log(global)
 console.log('Hello world2')
 
-const os = require('os')
-console.log(os.type())
-console.log(os.version())
-console.log(os.homedir())
+// const os = require('os')
+const {add, sub , mul, div } = require('./math')
 
-console.log(__dirname)
-console.log(__filename)
+console.log(add(2,3))
+console.log(sub(2,3))
+console.log(mul(2,3))
+console.log(div(4,2))
+
+
+
+
+
+
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
+
+// console.log(__dirname)
+// console.log(__filename)
